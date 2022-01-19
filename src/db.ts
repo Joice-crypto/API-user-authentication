160af8ab2dbbef1b2015371944ca08222c4210b4
Joice Cristina <66541373+Joice-crypto@users.noreply.github.com>
2022-01-19 02:16:46 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";  
 
 
-const connectionString = 'postgres://ubjpeufw:CFOUMOe_PgUXVDHYSbQ-0AbTbEaPwRy0@kesavan.db.elephantsql.com/ubjpeufw'
+const connectionString = 'postgres://xxxxxxxxxxxxxx'
 const db = new Pool({connectionString});
 
 
